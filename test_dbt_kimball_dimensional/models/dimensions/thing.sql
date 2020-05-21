6950a55a3105be62094306fba72d1f2201462b91
ethan knox <ethan.m.knox@gmail.com>
2020-05-21 14:32:34 -0400

    type 0,1,2,4,10

--- test_dbt_kimball_dimensional/models/dimensions/thing.sql
+++ test_dbt_kimball_dimensional/models/dimensions/thing.sql
@@ -4,17 +4,30 @@ source_data AS (
 	SELECT *
 	FROM prod_source.user_total_replay
 )
-,type_2 AS (
+,slowly_changing_dimensions_with_duplicates AS (
    SELECT 
- 	user_id AS natural_key
+
+	-- type 0
+	LAST_VALUE(account_created_at) OVER w AS account_created_at
+	
+	-- type 1
+	,FIRST_VALUE(email) OVER w AS current_email_address
+
+	-- type 4
+	, array_agg(email) OVER (partition by user_id ORDER BY batched_at) AS all_email_addresses
+	
+	-- type 10 is just type 4 + type 2
+
+	-- type 2
+ 	,user_id AS natural_key
 	,first_name
 	,last_name
 	,email
 	,phone_number
 	,preferred_status AS is_preferred_user
 	,birthdate
-	,account_created_at
 	
+
 	,case when LAST_VALUE(batched_at) over w = batched_at
 	THEN '1970-01-01'
 	ELSE batched_at
@@ -28,5 +41,35 @@ source_data AS (
 	from source_data
 	window w as (partition by user_id order by batched_at desc range between unbounded preceding and unbounded following )
 	order by user_id, batched_at
+),
+deduplicated_aggregates AS (
+    SELECT
+	natural_key
+	,array_agg(DISTINCT item) AS all_email_addresses
+   FROM slowly_changing_dimensions_with_duplicates
+	,unnest(all_email_addresses) as item
+   GROUP BY 1
 )
-SELECT * FROM type_2
+,slowly_changing_dimensions AS (
+SELECT 
+   scd.natural_key
+   ,scd.first_name
+   ,scd.last_name
+   ,email
+   ,phone_number
+   ,is_preferred_user
+   ,birthdate
+   ,row_effective_at
+   ,row_expired_at
+   ,row_is_current
+   ,dedupe.all_email_addresses AS all_email_addresses
+   ,scd.account_created_at
+   ,scd.current_email_address
+FROM 
+slowly_changing_dimensions_with_duplicates scd
+LEFT JOIN
+deduplicated_aggregates dedupe
+USING (natural_key)
+)
+
+SELECT * FROM slowly_changing_dimensions
